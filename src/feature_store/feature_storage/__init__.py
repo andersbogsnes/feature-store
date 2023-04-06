ec4a719d6c192a1fd282619001ff167d70168012
Anders Bogsnes <andersbogsnes@gmail.com>
2023-04-06 21:09:08 +0200

    Renamed backends to more clearly distinguish between registry and sto… (#34)
    * Renamed backends to more clearly distinguish between registry and storage
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/feature_store/feature_storage/__init__.py
@@ -0,0 +1,3 @@
+from feature_store.feature_storage.base import FeatureStorage
+
+__all__ = ["FeatureStorage"]
---
+++ src/feature_store/registry_backends/base.py
@@ -0,0 +1,15 @@
+from typing import Optional, Protocol
+
+from feature_store.feature import Feature
+
+
+class RegistryBackend(Protocol):
+    def add_feature_metadata(self, feature: Feature) -> None:
+        ...
+
+    def get_feature_metadata(self, feature_name: str) -> Optional[Feature]:
+        ...
+
+    def get_available_feature_metadata(self) -> list[Feature]:
+        """Get names of available feature"""
+        ...
