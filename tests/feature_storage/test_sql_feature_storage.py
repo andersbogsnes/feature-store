95a75391dc89453dc7b0d9baf8f52d872abeee0c
Anders Bogsnes <andersbogsnes@gmail.com>
2023-04-06 22:09:31 +0200

    Added demo notebooks to run through basic functionality

--- tests/feature_storage/test_sql_feature_storage.py
+++ tests/feature_storage/test_sql_feature_storage.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 from feature_store import Client
+from feature_store.feature import Feature
 
 
-def test_can_get_feature(client: Client, age_df: pd.DataFrame, age_sql_feature):
+def test_can_get_feature(
+    client: Client, age_df: pd.DataFrame, age_sql_feature: Feature
+):
     result = client.get_feature(age_sql_feature.name)
     pd.testing.assert_frame_equal(result.to_pandas(), age_df, check_like=True)
